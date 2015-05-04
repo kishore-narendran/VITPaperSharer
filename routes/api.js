6e7717a44cc86cccda01df0add506b97e4392161
kishore-narendran <kishore.narendran09@gmail.com>
2015-05-04 22:24:09 +0530

    Added support for type. Started listing files in database and handling duplicates.

--- routes/api.js
+++ routes/api.js
@@ -3,32 +3,74 @@ var path = require('path');
 var PDFDocument = require('pdfkit');
 var fs = require('fs');
 var crypto = require('crypto');
+var status = require(path.join(__dirname, '..', 'status'));
 var router = express.Router();
 
 var uploadPaperAction = function(req, res) {
 
     var numberOfFiles = parseInt(req.body.number_of_files);
-    var classNumber = req.body.class_number;
+    var classNumber = parseInt(req.body.class_number);
     var title = req.body.title;
     var code  = req.body.code;
     var slot = req.body.slot;
-    var semester = req.body.semester;
-    var year = parseInt(req.body.year);
-    var doc = new PDFDocument();
-    var pdfName = classNumber+ "-" + code + "-" + slot + "-" + semester + (year%100).toString();
-    doc.pipe(fs.createWriteStream('public/generated/'+ pdfName + '.pdf'));
-    for(var i = 0; i < numberOfFiles; i++) {
-        var fieldName = ('file' + ((i + 1).toString()));
-        var name = req.files[fieldName].name;
-        doc.image('public/uploads/' + name);
-        if (i != numberOfFiles - 1) {
-            doc.addPage();
+    var type = req.body.type;
+    var semester = req.body.semester + (parseInt(req.body.year) % 100).toString();
+
+    var generatePDF = function(numberOfPapers) {
+        var doc = new PDFDocument();
+        var pdfName = classNumber+ '-' + type + '-' + code + '-' + slot + '-' + semester + ' (' + (numberOfPapers+1) + ')';
+        doc.pipe(fs.createWriteStream('public/generated/'+ pdfName + '.pdf'));
+        for(var i = 0; i < numberOfFiles; i++) {
+            var fieldName = ('file' + ((i + 1).toString()));
+            var name = req.files[fieldName].name;
+            doc.image('public/uploads/' + name);
+            if (i != numberOfFiles - 1) {
+                doc.addPage();
+            }
+            fs.unlink('public/uploads/' + name);
         }
-        fs.unlink('public/uploads/' + name);
-    }
-    doc.end();
-    res.json({});
+        doc.end();
+        return ('public/generated/'+ pdfName + '.pdf');
+    };
+
+    var onInsert = function(err, result) {
+        if(err) {
+            res.json({result: status.failure});
+        }
+        else {
+            res.json({result: status.success});
+        }
+    };
+    var onUpdate = function(err, result) {
+        if(err) {
+            res.json({result: status.failure});
+        }
+        else {
+            res.json({result: status.success});
+        }
+    };
+    var onFindPapers = function(err, result) {
+        var fileName;
+        var files = [];
+        if(err) {
+            res.json({result: status.failure});
+        }
+        else if(result == null) {
+            fileName = generatePDF(0);
+            files.push(fileName);
+            req.db.collection('papers').insert({class_number: classNumber, title: title, code: code, slot: slot, type: type, semester: semester, files: files}, onInsert);
+        }
+        else {
+            var numberOfPapers = result.files.length;
+            files = result.files;
+            fileName = generatePDF(numberOfPapers);
+            files.push(fileName);
+            req.db.collection('papers').update({class_number: classNumber, title: title, code: code, slot: slot, type: type, semester: semester}, {$set: {files: files}}, {upsert: true}, onUpdate);
+        }
+    };
+    req.db.collection('papers').findOne({class_number: classNumber, code: code, slot: slot, type: type, semester: semester}, onFindPapers);
 };
+
 router.post('/uploadpaper', uploadPaperAction);
 
 module.exports = router;
---
+++ status.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var status = {
+    success: {
+        message: 'Successful execution',
+        code: 0
+    },
+    failure: {
+        message: 'Failure',
+        code: 1
+    }
+};
+
+module.exports = status;
